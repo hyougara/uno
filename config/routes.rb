5562e5e793a2d320056df1f805ee630fb53abf9d
前田優樹 <orzazous2@gmail.com>
2019-01-27 21:04:31 +0900

    お気に入り完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
   resources :posts do
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
